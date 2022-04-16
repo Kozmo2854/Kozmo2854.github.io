6953671d78146a541b0d0854d61f58bfaa832e5b
Kozmo2854 <jovanzeka97@gmail.com>
2022-04-16 21:31:31 +0200

    Contact fixing

--- contact.js
+++ contact.js
@@ -1,20 +1,52 @@
 window.onload = function () {
+    $(".alert-success").hide();
+
+    getJSONdata(writeMenu,'menu');
+
+
     let inputs = ["Name", "Email", "Tel", "Radio", "Message"];
     $(".btnForm").click(function () {
-        let errors=[];
+        let errors = [];
         for (input of inputs) {
             errors.push(checkInputs(input));
         }
-        console.log(errors.length)
+        if(!errors.includes(1)){
+            $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+                $("#success-alert").slideUp(500);
+            });
+        }
     })
 
     function checkInputs(inputName) {
-        console.log($(`.${inputName}`).val());
-        if (!$(`.${inputName}`).val() || (!$(`.${inputName}:checked`).length)) {
+        if ($(`.${inputName}`).val() == "" || (inputName == "Radio" && $(`.${inputName}:checked`).length == 0)) {
             $(`.displayError${inputName}`).show();
             return 1;
         } else {
             $(`.displayError${inputName}`).hide();
         }
+    }
+
+    function writeMenu(data){
+        ispis='<ul>';
+        data.forEach(menuItem=>{
+            ispis+=`
+                <li>
+                    <a href="${menuItem.href}">${menuItem.naziv}</a>
+                </li>
+        `
+        })
+        ispis+="</ul>"
+        $('.menu').html(ispis);
+    }
+
+    function getJSONdata(callbackFunction, file){
+        $.ajax({
+            url:"assets/JSONfiles/" + file + ".json",
+            type:"JSON",
+            method:"GET",
+            success:function (data) {
+                callbackFunction(data);
+            }
+        })
     }
 }
